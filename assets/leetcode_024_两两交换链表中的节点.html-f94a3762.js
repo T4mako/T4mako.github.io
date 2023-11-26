6c06abc7dfed1b8dd37e126c9b5b12c6c26c9031
T4mako <72336261+T4mako@users.noreply.github.com>
2023-11-26 15:35:42 +0000

    Deploying to gh-pages from @ T4mako/T4mako.github.io@854f6121d38dc3968c08f6e6ee1617094af2f9dd 🚀

---
+++ assets/leetcode_024_两两交换链表中的节点.html-f94a3762.js
@@ -0,0 +1,47 @@
+import{_ as a}from"./plugin-vue_export-helper-c27b6911.js";import{r as t,o as e,c as p,e as o,b as n,d as c,f as l}from"./app-cb39c25b.js";const i={},u=n("h1",{id:"_024-两两交换链表中的节点",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#_024-两两交换链表中的节点","aria-hidden":"true"},"#"),c(" 024_两两交换链表中的节点")],-1),d=l(`<div class="language-java line-numbers-mode" data-ext="java"><pre class="language-java"><code><span class="token doc-comment comment">/**
+ * Definition for singly-linked list.
+ * public class ListNode <span class="token punctuation">{</span>
+ *     int val;
+ *     ListNode next;
+ *     ListNode() <span class="token punctuation">{</span><span class="token punctuation">}</span>
+ *     ListNode(int val) <span class="token punctuation">{</span> this.val = val; <span class="token punctuation">}</span>
+ *     ListNode(int val, ListNode next) <span class="token punctuation">{</span> this.val = val; this.next = next; <span class="token punctuation">}</span>
+ * <span class="token punctuation">}</span>
+ */</span>
+<span class="token keyword">class</span> <span class="token class-name">Solution</span> <span class="token punctuation">{</span>
+    <span class="token keyword">public</span> <span class="token class-name">ListNode</span> <span class="token function">swapPairs</span><span class="token punctuation">(</span><span class="token class-name">ListNode</span> head<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">if</span><span class="token punctuation">(</span>head <span class="token operator">==</span> <span class="token keyword">null</span> <span class="token operator">||</span> head<span class="token punctuation">.</span>next <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span><span class="token punctuation">{</span>
+            <span class="token keyword">return</span> head<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span>
+        <span class="token class-name">ListNode</span> temp <span class="token operator">=</span> head<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+        <span class="token keyword">if</span><span class="token punctuation">(</span>head<span class="token punctuation">.</span>next<span class="token punctuation">.</span>next <span class="token operator">!=</span> <span class="token keyword">null</span><span class="token punctuation">)</span><span class="token punctuation">{</span>
+            head<span class="token punctuation">.</span>next <span class="token operator">=</span> head<span class="token punctuation">.</span>next<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span><span class="token keyword">else</span><span class="token punctuation">{</span>
+            head<span class="token punctuation">.</span>next<span class="token punctuation">.</span>next <span class="token operator">=</span> head<span class="token punctuation">;</span>
+            head <span class="token operator">=</span> head<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+            head<span class="token punctuation">.</span>next<span class="token punctuation">.</span>next <span class="token operator">=</span> <span class="token keyword">null</span><span class="token punctuation">;</span>
+            <span class="token keyword">return</span> head<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span>
+        temp<span class="token punctuation">.</span>next <span class="token operator">=</span> head<span class="token punctuation">;</span>
+        head <span class="token operator">=</span> temp<span class="token punctuation">;</span>
+        <span class="token class-name">ListNode</span> i  <span class="token operator">=</span> head<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+        <span class="token keyword">while</span><span class="token punctuation">(</span>i<span class="token punctuation">.</span>next <span class="token operator">!=</span> <span class="token keyword">null</span> <span class="token operator">&amp;&amp;</span> i<span class="token punctuation">.</span>next<span class="token punctuation">.</span>next <span class="token operator">!=</span> <span class="token keyword">null</span><span class="token punctuation">)</span><span class="token punctuation">{</span>
+            <span class="token class-name">ListNode</span> t3 <span class="token operator">=</span> i<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+            <span class="token class-name">ListNode</span> t4 <span class="token operator">=</span> i<span class="token punctuation">.</span>next<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+            <span class="token class-name">ListNode</span> t5 <span class="token operator">=</span> <span class="token keyword">null</span><span class="token punctuation">;</span>
+            <span class="token keyword">if</span><span class="token punctuation">(</span>t4<span class="token punctuation">.</span>next <span class="token operator">!=</span> <span class="token keyword">null</span><span class="token punctuation">)</span><span class="token punctuation">{</span>
+                t5 <span class="token operator">=</span> t4<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+            <span class="token punctuation">}</span>
+            i<span class="token punctuation">.</span>next <span class="token operator">=</span> t4<span class="token punctuation">;</span>
+            t4<span class="token punctuation">.</span>next <span class="token operator">=</span> t3<span class="token punctuation">;</span>
+            <span class="token keyword">if</span><span class="token punctuation">(</span>t5 <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span><span class="token punctuation">{</span>
+                t3<span class="token punctuation">.</span>next <span class="token operator">=</span> <span class="token keyword">null</span><span class="token punctuation">;</span>
+                <span class="token keyword">return</span> head<span class="token punctuation">;</span>
+            <span class="token punctuation">}</span>
+            t3<span class="token punctuation">.</span>next <span class="token operator">=</span> t5<span class="token punctuation">;</span>
+            i <span class="token operator">=</span> i<span class="token punctuation">.</span>next<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span>
+        <span class="token keyword">return</span> head<span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>将链表元素为0，链表元素为1的情况直接返回原来数组即可，当元素为2，改变连个节点元素的指针指向， 并返回，如果元素节点大于等于三个，设置一个指针i，指向第双数个元素节点，通过临时变量将元素i的洗衣歌，下下一个，下下下一个元素记录，并改变它们的指针指向，然后将i指针后移两个，直到指针的下一个元素节点或者下下一个元素节点都不存在时，结束循环，返回head指针。</p>`,2);function k(r,v){const s=t("Badge");return e(),p("div",null,[u,o(s,{text:"中等",type:"warning",vertical:"middle"}),d])}const x=a(i,[["render",k],["__file","leetcode_024_两两交换链表中的节点.html.vue"]]);export{x as default};
